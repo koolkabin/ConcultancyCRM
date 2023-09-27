853ccf90a198079ec0d29c32d0f32e6a25a052e7
AnzitGC741 <anzit.gc@antsware.com>
2023-09-27 23:27:49 +0545

    New module Assests Category added

---
+++ ConcultancyCRM/Controllers/AssestCategory.cs
@@ -0,0 +1,18 @@
+﻿using ConcultancyCRM.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConcultancyCRM.Controllers
+{
+    public class AssestCategory : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET: AssestCategory/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ ConcultancyCRM/Models/AssestCategory.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ConcultancyCRM.Models
+{
+    public class AssestCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Title { get; set; }
+        [MaxLength(255)]
+        public string Description { get; set; }
+        [UIHint("ActiveInActive")]
+        public bool Status { get; set; }
+        [UIHint("YesNo")]
+        public bool Deleted { get; set; }
+        public DateTime LastUpdatedByDate { get; set; }
+        public DateTime LastUpdatedByName { get; set; }
+    }
+}
